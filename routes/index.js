058e049a450864af4b615a10a008da1d0fdaba0d
Eric <eric.amram@gmail.com>
2015-12-06 03:03:03 -0800

    Redirect

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. * /
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hour of Code' });
+}); */
+router.get('/', function(req, res, next) {
+  res.redirect('main.html');
 });
 
 module.exports = router;
